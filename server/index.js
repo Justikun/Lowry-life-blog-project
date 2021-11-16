614b523c3031ea48d66a506706878c0f34f48dd9
Justin Lowry <github@lowry.life>
2021-11-16 01:25:00 -0700

    database working

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = require('dotenv')
-const {seed} = require('./seed')
-//destructure functions here
+const {SERVER_PORT} = process.env
+const {seed} = require('./seed.js')
 
 app.use(express.json())
 app.use(cors())
+
+// DEV
+app.post('/seed', seed)
+app.listen(SERVER_PORT, () => console.log(`Up on ${SERVER_PORT}`))
--- server/seed.js
+++ server/seed.js
@@ -2,96 +2,70 @@
 require('dotenv').config()
 const {CONNECTION_STRING} = process.env
 const Sequelize = require('sequelize')
+
 const sequelize = new Sequelize(CONNECTION_STRING, {
-    dialect: "postgres",
+    dialect: 'postgres', 
     dialectOptions: {
         ssl: {
             rejectUnauthorized: false
         }
     }
-});
+})
 
 
 module.exports = {
-    seed:(req, res) => {
+    seed: (req, res) => {
         sequelize.query(/*sql*/`
         
-        DROP TABLE IF EXISTS public.Users;
-        DROP TABLE IF EXISTS public.Posts;
-        DROP TABLE IF EXISTS public.Auth;
-        DROP TABLE IF EXISTS public.Home_Screen;
-        DROP TABLE IF EXISTS public.Profile_Page;
+        DROP TABLE IF EXISTS Auth cascade;
+        DROP TABLE IF EXISTS Users cascade;
+        DROP TABLE IF EXISTS Posts cascade;
+        DROP TABLE IF EXISTS Home_Screen cascade;
+        DROP TABLE IF EXISTS Profile_Page cascade;
 
-        CREATE TABLE "public.Users" (
-            "user_id" serial NOT NULL,
+        CREATE TABLE Users (
+            "user_id" serial PRIMARY KEY NOT NULL,
             "first_name" varchar(100) NOT NULL,
             "last_name" varchar(100) NOT NULL,
             "bio" varchar(5000) NOT NULL,
-            "quote" varchar(225),
-            CONSTRAINT "Users_pk" PRIMARY KEY ("user_id")
-        ) WITH (
-        OIDS=FALSE
+            "quote" varchar(225)
         );
 
-        CREATE TABLE "public.Posts" (
-            "post_id" serial NOT NULL,
-            "user_id" integer NOT NULL,
+        CREATE TABLE Posts (
+            "post_id" serial PRIMARY KEY NOT NULL,
+            "user_id" integer NOT NULL REFERENCES Users(user_id),
             "post_title" varchar(255) NOT NULL,
-            "post_text" varchar NOT NULL,
-            "date" DATE NOT NULL,
-            CONSTRAINT "Posts_pk" PRIMARY KEY ("post_id")
-        ) WITH (
-        OIDS=FALSE
+            "post_text" varchar(10000) NOT NULL,
+            "date" DATE NOT NULL
         );
 
-
-
-        CREATE TABLE "public.Auth" (
-            "user_id" integer NOT NULL,
+        CREATE TABLE Auth (
+            "user_id" integer PRIMARY KEY NOT NULL REFERENCES Users(user_id),
             "password" varchar(255) NOT NULL,
-            "email" varchar(255) NOT NULL UNIQUE,
-            CONSTRAINT "Auth_pk" PRIMARY KEY ("user_id")
-        ) WITH (
-        OIDS=FALSE
+            "email" varchar(255) NOT NULL UNIQUE
         );
 
-
-
-        CREATE TABLE "public.Home_Screen" (
-            "home_id" serial NOT NULL,
-            "post_id" integer NOT NULL,
-            CONSTRAINT "Home_Screen_pk" PRIMARY KEY ("home_id")
-        ) WITH (
-        OIDS=FALSE
+        CREATE TABLE Home_Screen (
+            "home_id" serial PRIMARY KEY NOT NULL,
+            "post_id" integer NOT NULL REFERENCES Posts(post_id)
         );
 
-
-
-        CREATE TABLE "public.Profile_Page" (
-            "user_id" integer NOT NULL,
-            "post_id" integer NOT NULL,
-            CONSTRAINT "Profile_Page_pk" PRIMARY KEY ("user_id")
-        ) WITH (
-        OIDS=FALSE
+        CREATE TABLE Profile_Page (
+            "user_id" integer PRIMARY KEY NOT NULL,
+            "post_id" integer NOT NULL REFERENCES Posts(post_id)
         );
 
-        ALTER TABLE "Users" ADD CONSTRAINT "Users_fk0" FOREIGN KEY ("bio") REFERENCES ""("");
-        ALTER TABLE "Users" ADD CONSTRAINT "Users_fk1" FOREIGN KEY ("quote") REFERENCES ""("");
+        -- Inserting test users
+        INSERT INTO Users (first_name, last_name, bio, quote)
+        VALUES ('Justin', 'Lowry', 'Here to blog my life as a software engineer!', 'And thats the thing about life, you get to create it, design it, and live it.');
+        
+        -- Inserting test posts
+        INSERT INTO Posts (user_id, post_title, post_text, date)
+        VALUES (1, 'I went to the Silicon Slopes tech conference today! Here is what I learned', 'Okay. This is the text content of the blog post', '2021-11-16');
 
-        ALTER TABLE "Posts" ADD CONSTRAINT "Posts_fk0" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id");
-
-        ALTER TABLE "Auth" ADD CONSTRAINT "Auth_fk0" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id");
-
-        ALTER TABLE "Home_Screen" ADD CONSTRAINT "Home_Screen_fk0" FOREIGN KEY ("post_id") REFERENCES "Posts"("post_id");
-
-        ALTER TABLE "Profile_Page" ADD CONSTRAINT "Profile_Page_fk0" FOREIGN KEY ("user_id") REFERENCES "Users"("user_id");
-        ALTER TABLE "Profile_Page" ADD CONSTRAINT "Profile_Page_fk1" FOREIGN KEY ("post_id") REFERENCES "Posts"("post_id");
-
-        `)
-        .then(() => {
+        `).then(() => {
             console.log("DB SUCCESSFULLY SEEDED!")
             res.sendStatus(200)
-        })
-        .catch(err => console.log("ERROR SEEDING DB", err))
+        }).catch(err => console.log("ERROR SEEDING DB", err))
     }
 }
