80bb0bfb8b5d016de0bd8a9b3c7740ccdbb68bb9
Justin Lowry <github@lowry.life>
2021-11-17 14:32:14 -0700

    moved lines

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
+
 const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const {SERVER_PORT} = process.env
 const {seed} = require('./seed.js')
+
+app.use(express.json())
+app.use(cors())
 
 // For Heroku to serve my js and css
 app.get('/', (req, res) => {
@@ -14,15 +18,8 @@ app.get('/', (req, res) => {
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, './public/main.js'))
 })
-
-
-
-app.use(express.json())
-app.use(cors())
-
 // DEV
 app.post('/seed', seed)
-
 
 const port = process.env.PORT || SERVER_PORT
 app.listen(port, () => console.log(`Up on ${port}`))
