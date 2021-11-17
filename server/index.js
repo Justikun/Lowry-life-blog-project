280a9bd270ba585298642d2c3ec5af2589fcf0b5
Justin Lowry <github@lowry.life>
2021-11-17 14:40:04 -0700

    removed references

--- server/index.js
+++ server/index.js
@@ -15,9 +15,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../pages/home/home.html'))
 })
 
-app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
-})
+// app.get('/js', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/main.js'))
+// })
 // DEV
 app.post('/seed', seed)
 
