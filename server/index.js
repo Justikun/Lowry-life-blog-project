66b82ef93e3e09adc49466577c5610c7ce128e60
Justin Lowry <github@lowry.life>
2021-11-17 14:04:02 -0700

    port

--- server/index.js
+++ server/index.js
@@ -3,17 +3,18 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
+const {PORT} = process.env
 const {seed} = require('./seed.js')
 
+// For Heroku to serve my js and css
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-    
+
 app.use(express.json())
 app.use(cors())
 
 // DEV
 app.post('/seed', seed)
 
-app.listen(SERVER_PORT, () => console.log(`Up on ${SERVER_PORT}`))
+app.listen(PORT, () => console.log(`Up on ${PORT}`))
